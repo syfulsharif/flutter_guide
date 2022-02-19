c7a55861a9655b4225433e14e73ac973afa6dea5
syfulsharif <sharif.syful@gmail.com>
2022-02-19 20:39:36 +0600

    Arrow Function Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(myApp());
+// }
+void main() => runApp(myApp());
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello!'),
+    );
+  }
+}
