f5240b3e3c2948fc09c9ac7707e142bad2fcee42
syfulsharif <sharif.syful@gmail.com>
2022-02-22 22:30:05 +0600

    Buttons Wired Up!

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(myApp());
 
 class myApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer Chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +17,23 @@ class myApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Rodeo'),
         ),
-        body: Text('An App for bikers'),
+        body: Column(
+          children: [
+            Text('The Question: '),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            )
+          ],
+        ),
       ),
     );
   }
